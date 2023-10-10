02a5c5b5464b299946098e26c555741ef9633b5b
Muhammad-Ahmed-Qazi <qmuhammadahmed@gmail.com>
2023-10-10 05:51:46 +0500

    Revised RPN algorithm.

--- Other algorithms/Reverse Polish Notation/algorithm.py
+++ Other algorithms/Reverse Polish Notation/algorithm.py
@@ -1,71 +1,84 @@
+from colorama import Fore, Back, Style, init
+init()
+
 import os, sys
 print("Your current working directory is", os.getcwd())
 curr = input("Enter the path to the folder containing the classes: ")
-print(os.path.join(str(os.getcwd()) + str(curr)))
-curr = os.path.join(str(os.getcwd()) + str(curr)) # Path to classes folder
+# print(os.path.join(str(os.getcwd()) + str(curr)))
+curr = os.path.join(str(os.getcwd()) + str(curr))
 sys.path.insert(0, curr)
-import stack as stClass # Importing stack class
+import stack as stClass
 
-operators = {'+': 2, '-': 1, '*': 3, '/': 4} # Dictionary of operators and their precedence according to BODMAS
+operators = {'+': 2, '-': 1, '*': 3, '/': 4}
 brackets = ['(', ')']
 
 userChoice = 0
+postfix = None
 
-# Function to check if the current operator has higher precedence than the operator at the top of the stack
+
 def isHigherPrecedent(opCurr, opAgainst):
     if operators[opCurr] < operators[opAgainst] or operators[opCurr] == operators[opAgainst]:
         return True
     else:
         return False
 
-# Function to convert infix expression to postfix expression
+
 def infixToPostfix(exp):
-    stack = stClass.MyStack(len(exp)) # Creating a stack object
-    result = "" # String to store the postfix expression
-
-    # Iterating through the expression
+    exp = exp.replace(' ', '')
     for i in range(len(exp)):
-        curr = exp[i]
+        if exp[i] not in operators.keys() and exp[i] not in brackets and exp[i + 1] == '(':
+            closeIndex = exp.find(')', i)
+            exp = exp[:i] + '(' + exp[i] + '*' + exp[i + 1:closeIndex] + ')' + exp[closeIndex:]
 
-        # Current character is an operand
+    print(exp)
+
+    stack = stClass.MyStack(len(exp))
+    result = ""
+
+    i = 0
+    while i < len(exp):
+        curr = exp[i]
+        try:
+            if curr not in operators.keys() and curr != '(' and curr != ')' and curr != ' ':
+                if exp[i + 1] != ' ' and exp[i + 1] not in operators.keys() and exp[i + 1] not in brackets:
+                    curr += exp[i + 1]
+                    i += 1
+        except:
+            pass
+
         if curr not in operators.keys() and curr not in brackets:
             result += curr + " "
 
-        # Current character is an operator
         elif curr in operators.keys():
             popped = stack.pop()
 
-            # If the stack is not empty and the operator at the top of the stack has higher precedence than the current operator
             while popped != False and popped != '(' and isHigherPrecedent(curr, popped):
-                result += popped + " " # Add the operator at the top of the stack to the result string
+                result += popped + " " 
                 popped = stack.pop()
 
             if popped in operators.keys() and not isHigherPrecedent(curr, popped):
-                stack.push(popped) # Push the operator back to the stack
+                stack.push(popped)
             elif popped == '(':
-                stack.push(popped) # Push the operator back to the stack
+                stack.push(popped)
 
-            stack.push(curr) # Push the current operator to the stack
+            stack.push(curr)
 
-        # Current character is an opening bracket
         elif curr == '(':
             stack.push(curr)
 
-        # Current character is a closing bracket
         elif curr == ')':
             popped = stack.pop()
 
-            # Pop all the operators from the stack until an opening bracket is encountered
             while popped != False and popped != '(':
                 result += popped + " "
                 popped = stack.pop()
 
-        # print(result)
-        # stack.display()
+        print(result)
+        stack.display()
+        i += 1
 
     popped = stack.pop()
 
-    # Pop all the remaining operators from the stack
     while popped != False:
         result += popped + " "
         popped = stack.pop()
@@ -73,18 +86,66 @@ def infixToPostfix(exp):
     return result
 
 def evaluatePostfix(exp):
+    i = 0
     stack = stClass.MyStack(len(exp))
-    
 
-while userChoice != 2:
-    print("1. Convert infix expression to postfix expression")
-    print("2. Exit")
+    while i < len(exp):
+        curr = exp[i]
+        try:
+            if curr not in operators.keys() and curr != '(' and curr != ')' and curr != ' ':
+                if exp[i + 1] != ' ' and exp[i + 1] not in operators.keys() and exp[i + 1] not in brackets:
+                    curr += exp[i + 1]
+                    i += 1
+        except:
+            pass
+        
+        if curr not in operators.keys() and curr != ' ':
+            stack.push(curr)
+        elif curr != ' ':
+            b = float(stack.pop())
+            a = float(stack.pop())
+
+            
+            try:
+                if curr == '+': result = a + b
+                elif curr == '-': result = a - b
+                elif curr == '*': result = a * b
+                elif curr == '/': result = a / b
+            except:
+                print(Fore.RED + "Expression is undecided! It cannot be solved!", Style.RESET_ALL)
+                break
+            
+            try:
+                result = str(float(result))
+            except:
+                result = str(result)
+            
+            stack.push(result)
+            
+        i += 1
+    
+    return stack.pop()
+
+while userChoice != 3:
+    print(Fore.BLUE + "1. Convert infix expression to postfix expression", Style.RESET_ALL)
+    print(Fore.BLUE + "2. Evaluate postfix expression", Style.RESET_ALL)
+    print(Fore.BLUE + "3. Exit", Style.RESET_ALL)
     userChoice = int(input("Enter your choice: "))
 
     if userChoice == 1:
-        exp = input("Enter the expression: ")
-        print("Postfix expression:", infixToPostfix(exp))
+        exp = input("Enter the infix expression: ")
+        postfix = infixToPostfix(exp)
+        print(Fore.GREEN + "Postfix expression:", postfix, Style.RESET_ALL)
     elif userChoice == 2:
-        print("Thank you for using the program!")
+        if postfix != None:
+            useCalculated = input("Do you want the above converted infix expression to be evaluated? (y/N)\n>>> ")
+            if useCalculated == 'y': print(Fore.GREEN + "Postfix expression:", evaluatePostfix(postfix), Style.RESET_ALL)
+        else:
+            exp = input("Enter the postfix expression: ")
+            print(Fore.GREEN + "Postfix expression:", evaluatePostfix(exp), Style.RESET_ALL)
+    elif userChoice == 3:
+        print(Fore.CYAN + "Thank you for using the program!", Style.RESET_ALL)
     else:
-        print("Invalid choice! Try again.\n")
+        print(Fore.RED + "Invalid choice! Try again.\n", Style.RESET_ALL)
+
+    print("")
